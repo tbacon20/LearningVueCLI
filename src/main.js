efdf2f7bbdf50151a9f17b74a742ed7664a16b44
tbacon20 <tanner@DESKTOP-5EJR3IC.localdomain>
2022-08-06 22:28:39 -0400

    Repository Upload 8/6/2022

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+let data = {
+  currentID: 2,
+  tickets: [{
+    id: 1,
+    problem: 'This app is not completely written yet.',
+    name: 'Emma'
+  }],
+  getTickets() {
+    return this.tickets;
+  },
+  addTicket(name, problem) {
+    this.tickets.push({
+      id: this.currentID,
+      name: name,
+      problem: problem
+    });
+    this.currentID += 1;
+  }
+}
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  data: data,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h1>Current Tickets</h1>
+  <div v-for="ticket in tickets" v-bind:key="ticket.id">
+    <hr/>
+    <div class="ticket">
+      <div class="problem">
+        <p>{{ ticket.problem }}</p>
+        <p><i>-- {{ ticket.name }}</i></p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {}
+  },
+  computed: {
+    tickets() {
+      return this.$root.$data.getTickets();
+    }
+  }
+};
+</script>
